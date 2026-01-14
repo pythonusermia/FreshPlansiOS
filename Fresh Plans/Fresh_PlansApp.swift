fbe42109a488bfd48ecf3f8e78dad38cc7b23603
Mia Yonker <mia.yonker@me.com>
2026-01-14 16:16:14 -0600

    Initial Commit

---
+++ Fresh Plans/Fresh_PlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fresh_PlansApp.swift
+//  Fresh Plans
+//
+//  Created by Mia Yonker on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Fresh_PlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
